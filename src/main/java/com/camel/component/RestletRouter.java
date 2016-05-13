b7fd19e9e2a6c957ff4fce8f809099485c3e3f28
Christian Arias <christian.arias@qualifacts.com>
2016-05-13 17:17:03 -0500

    Remove unused comments

--- src/main/java/com/camel/component/RestletRouter.java
+++ src/main/java/com/camel/component/RestletRouter.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 public class RestletRouter extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
-//		from("restlet:http://localhost:8081/hello?restletMethod=post").setBody().simple("hello world")
 		from("restlet:/hello?restletMethod=post").setBody().simple("hello world")
 		.to("direct:hello");
 		from("direct:hello").transform().constant("hello world");
